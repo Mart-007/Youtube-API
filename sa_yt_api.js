8ccb46cb691c728bdd2a5180870f2fe62b32519a
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-22 10:10:46 +0800

    more interpolations haha

--- sa_yt_api.js
+++ sa_yt_api.js
@@ -1,9 +1,11 @@
+const https = require('https')
+
+
 channel_id = "UCYc2dDPuAzbySHNj-CzLckQ"
 key = "AIzaSyAmfuCVpImQT3vUBZD5CRA6z-urx5XM8GY" // api key ko to sir haha palitan nyo nalang ng inyo if u want- jerome
+count = 3
+url = `https://youtube.googleapis.com/youtube/v3/search?part=id&channelId=${channel_id}&maxResults=${count}&order=relevance&key=${key}`
 
-url = `https://youtube.googleapis.com/youtube/v3/search?part=id&channelId=UCYc2dDPuAzbySHNj-CzLckQ&maxResults=3&order=relevance&key=${key}`
-
-const https = require('https')
 
 const req = https.request(url, res => {
   console.log(`statusCode: ${res.statusCode}`)
