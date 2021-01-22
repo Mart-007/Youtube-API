777a731a229e1b1decfd4c65faf54f091f7d2874
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-22 10:24:12 +0800

    add part and order param

--- sa_yt_api.js
+++ sa_yt_api.js
@@ -5,7 +5,9 @@ channel_id = "UCYc2dDPuAzbySHNj-CzLckQ"
 key = "AIzaSyAmfuCVpImQT3vUBZD5CRA6z-urx5XM8GY" // api key ko to sir haha palitan nyo nalang ng inyo if u want- jerome
 count = 3
 subject = "math"
-url = `https://youtube.googleapis.com/youtube/v3/search?part=id&channelId=${channel_id}&maxResults=${count}&q=${subject}&order=relevance&key=${key}`
+part = "id"
+order = "relevance"
+url = `https://youtube.googleapis.com/youtube/v3/search?part=${part}&channelId=${channel_id}&maxResults=${count}&q=${subject}&order=${order}&key=${key}`
 
 const req = https.request(url, res => {
   console.log(`statusCode: ${res.statusCode}`)
