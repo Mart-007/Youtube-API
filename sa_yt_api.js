2463366668a758bb9a3d4ffaa32146480ba0d59e
Jerome Lalunio <josejeromelalunio@gmail.com>
2021-01-22 10:18:19 +0800

    added subject param

--- sa_yt_api.js
+++ sa_yt_api.js
@@ -4,8 +4,8 @@ const https = require('https')
 channel_id = "UCYc2dDPuAzbySHNj-CzLckQ"
 key = "AIzaSyAmfuCVpImQT3vUBZD5CRA6z-urx5XM8GY" // api key ko to sir haha palitan nyo nalang ng inyo if u want- jerome
 count = 3
-url = `https://youtube.googleapis.com/youtube/v3/search?part=id&channelId=${channel_id}&maxResults=${count}&order=relevance&key=${key}`
-
+subject = "math"
+url = `https://youtube.googleapis.com/youtube/v3/search?part=id&channelId=${channel_id}&maxResults=${count}&q=${subject}&order=relevance&key=${key}`
 
 const req = https.request(url, res => {
   console.log(`statusCode: ${res.statusCode}`)
