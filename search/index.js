670d066ed1fe1c3b15356e126fedb0991fd79bcf
Reymart Bordo <62002257+Mart-007@users.noreply.github.com>
2021-02-15 08:42:50 +0800

    first commit

--- search/index.js
+++ search/index.js
@@ -1,6 +1,40 @@
 const youtube = require('scrape-youtube').default;
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const url = "mongodb+srv://admin:admin1234@cluster0.g4gtp.mongodb.net/form?retryWrites=true&w=majority";
+
+mongoose.connect(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+//schema
+const dtaSchema = new mongoose.Schema (
+  {
+    data: Object,
+  },
+  {
+    collection: "score"
+  }
+)
+
+//define model
+const Dta = mongoose.model("Dta", dtaSchema);
+
+//Store Data
+const dtaData = (bodyData) => {
+  Dta({ data: bodyData }).save((err) => {
+    if (err) {
+      throw err;
+    }
+  });
+};
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
+
 
 const PORT = process.env.PORT || 5000;
 
@@ -15,6 +49,14 @@ youtube.search('Team Lyqa/Algebra Fundamentals').then(results => {
      
   });
 });
+
+//post here
+app.post("/", urlencodedParser, (req, res) => {
+  dtaData(req.body);
+  res.render("Goodluck!", results.videos.map((vid) => ({id: vid.id, title: video.title}))
+  );
+});
+
 
 app.listen(PORT, ()=> {
   console.log(`Server is running on port ${PORT}`);
